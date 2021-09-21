03d6e529bced6d5f526d4976b0f9d31458fc08af
loocy21 <shobharaj860@gmail.com>
2021-09-21 19:25:35 +0530

    This is my project

---
+++ Comcast_Selenium_Frm/src/test/java/Comcast/contact_Test/contact_Test.java
@@ -0,0 +1,21 @@
+package Comcast.contact_Test;
+
+import org.testng.annotations.Test;
+
+public class contact_Test {
+	@Test
+	public void createContactTest() {
+		System.out.println("execute createContactTest");
+	}
+	@Test
+	public void createContactwithOrgTest() {
+		System.out.println("execute createContactWithOrg");
+	}
+
+	@Test
+	public void deleteContactTest() {
+		System.out.println("execute deleteContactTest");
+	}
+
+
+}
